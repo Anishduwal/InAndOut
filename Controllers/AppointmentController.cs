e259fcf1aa1bedffaf44e2079359ed04b9ba0c83
unknown <anish.dwl1.ad@gmail.com>
2021-07-14 14:10:07 +0545

    CRUD operations on ASP.net core MVC

---
+++ Controllers/AppointmentController.cs
@@ -0,0 +1,19 @@
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+using InAndOut.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+    }
+}
